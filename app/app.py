7606e1f9ae99385d3d3152ceb8391b36005146cb
bluepost59 <kaput.kat@gmail.com>
2021-12-03 23:29:40 +0900

    mongoDBにアクセス時刻を投稿できるようにした

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 import flask
 import pymongo
+import time as t
+
 
 app = flask.Flask(__name__)
 
@@ -8,6 +10,10 @@ app = flask.Flask(__name__)
 def app_route():
     client = pymongo.MongoClient("my_docker_mongo", 27017)
     test_db = client.test_db
+    times_collection = test_db.times_collection
+
+    times_collection.insert_one({"time": t.time()})
+
     client.close()
     return "<h1>this is flask test page</h1>"
 
